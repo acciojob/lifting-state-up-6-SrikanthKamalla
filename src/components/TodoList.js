c5dbdb412b38ba2a4445138c3a32320459de6269
Srikanth Kamalla <srikanthkamalla2919@gmail.com>
2025-05-02 13:11:11 +0530

    push2

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,6 @@ import React from "react";
 const TodoList = ({ todos, handleComplete }) => {
   return (
     <div>
-      <h2>Child Component</h2>
       <ul>
         {todos.map((ele) => (
           <li key={ele.task}>
