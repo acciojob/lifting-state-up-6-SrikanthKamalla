32c11c9f403151ccfc093908bef81271369ab4d2
Srikanth Kamalla <srikanthkamalla2919@gmail.com>
2025-05-10 10:57:57 +0530

    push 2

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,8 @@ import "./../styles/App.css";
 const App = () => {
   const [todos, setTodos] = useState([
     { task: "Learn React", completed: false },
-    { task: "Build a project", completed: false },
-    { task: "Apply for jobs", completed: false },
+    { task: "Build a React app", completed: false },
+    { task: "Deploy the React app ", completed: false },
   ]);
 
   const handleComplete = (todo) => {
